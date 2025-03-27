17a9e3191a72cbfd183651a857399861ef051be7
nikoflame <piercenik@gmail.com>
2025-03-27 13:00:46 -0700

    mobile routing fix

--- app/routes.ts
+++ app/routes.ts
@@ -1,5 +1,5 @@
 import { type RouteConfig, route } from "@react-router/dev/routes";
 
 export default [
-  route("/", "./routes/home_mobile.tsx"),
+  route("/", "./routes/home.tsx"),
 ] satisfies RouteConfig;
